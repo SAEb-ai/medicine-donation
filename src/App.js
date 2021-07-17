cbfb588f560276b5655324b25f7ce3ef45d8dd27
Md Shahbaz Alam <shahbazalam75508@gmail.com>
2021-07-17 13:32:41 +0530

    Navbar Improved

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { useState } from "react";
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
 function App() {
+  const [hover, setHover] = useState(false);
   return (
     <>
       <div className="App">
         <Router>
-          <Navbar />
+        <Navbar />
         </Router>
       </div>
     </>
